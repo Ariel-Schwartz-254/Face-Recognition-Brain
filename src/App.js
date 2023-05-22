2c7f95fe9f452fac018464a749d7bcafd8347ef3
Ariel Schwartz <schwartz254@gmail.com>
2023-05-22 14:58:01 +0300

    Moving API call to the server

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ import Rank from './Components/Rank/Rank';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import './App.css';
 
-const USER_ID = 'schwartz254';
-// Your PAT (Personal Access Token) can be found in the portal under Authentification
-const PAT = 'a2b6797e58284e77b82fe367c013b00c';
-const APP_ID = 'my-first-application';
-// Change these to whatever model and image URL you want to use
-const MODEL_ID = 'face-detection';
-// const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
-
 const initialState = {
     input: '',
     imageUrl: '',
@@ -73,39 +65,13 @@ class App extends Component {
     onButtonSubmit = () => {
         this.setState({imageUrl: this.state.input})
 
-        ///////////////////////////////////////////////////////////////////////////////////
-        // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-        ///////////////////////////////////////////////////////////////////////////////////
-
-        const raw = JSON.stringify({
-            "user_app_id": {
-                "user_id": USER_ID,
-                "app_id": APP_ID
-            },
-            "inputs": [
-                {
-                    "data": {
-                        "image": {
-                            "url": this.state.input
-                        }
-                    }
-                }
-            ]
-        });
-
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-             'Accept': 'application/json',
-             'Authorization': 'Key ' + PAT
-            },
-            body: raw
-        };
-        // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-        // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-        // this will default to the latest version_id
-
-        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        fetch('http://localhost:3000/imageurl', {
+                    method: 'post',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        input: this.state.input
+                    })
+                })
         .then((response) => response.json())
         .then((result) => {
             this.displayFaceBox(this.calculateFaceLocation(result));
